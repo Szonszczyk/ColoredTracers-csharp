0ebd7b8a4bf8b56be484f5a0b6c585dafe5d595e
Szonszczyk <Szonszczyk@users.noreply.github.com>
2025-10-21 15:28:34 +0200

    Updated ModMetadata

--- ModMetadata.cs
+++ ModMetadata.cs
@@ -4,7 +4,7 @@ namespace ColoredTracers
 {
     public record ModMetadata : AbstractModMetadata
     {
-        public override string ModGuid { get; init; } = "szonszczyk.coloredtracers";
+        public override string ModGuid { get; init; } = "com.szonszczyk.coloredtracers";
         public override string Name { get; init; } = "ColoredTracers";
         public override string Author { get; init; } = "Szonszczyk";
         public override List<string>? Contributors { get; init; }
@@ -12,7 +12,7 @@ namespace ColoredTracers
         public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.0");
         public override List<string>? Incompatibilities { get; init; } = [];
         public override Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; } = [];
-        public override string? Url { get; init; } = ""; // TODO
+        public override string? Url { get; init; } = "https://github.com/Szonszczyk/ColoredTracers-csharp";
         public override bool? IsBundleMod { get; init; } = false;
         public override string? License { get; init; } = "MIT";
     }
